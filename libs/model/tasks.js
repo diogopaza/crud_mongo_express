e60410717df4fdb8ecb6a6df59d745eb40b23979
Diogo Paza <diogopazacvel@gmail.com>
2018-01-15 10:54:05 -0200

    inicio

---
+++ libs/model/tasks.js
@@ -0,0 +1,19 @@
+
+
+module.exports = function(){
+
+    var db = require('./../connect_db')();
+    var Schema = require('mongoose').Schema;
+
+    var task = Schema({
+        title:String,
+        description:String,
+        status:Boolean,
+        
+
+    });
+
+    return db.model('tasks',task);
+
+
+}
---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var model = require('./../libs/model/tasks')()
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+ 
+ model.find(null, function(err, tasks){
+    if(err){
+      throw err;
+    }
+    res.render('index', { title: 'Express', tasks: tasks } );
+ })
+ 
+
+ 
+});
+
+router.post('/add', function(req,res,next){
+  var body = req.body;
+  body.status = false;
+  model.create(body, function(err, task){
+    if(err){
+      throw err;
+    }
+    res.redirect('/');
+  })
+
+})
+
+module.exports = router;
